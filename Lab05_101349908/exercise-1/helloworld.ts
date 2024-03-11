06e2ad7332212e13c5386a7b32c46a24dbd2e54b
Alvaro-01 <123744213+Alvaro-01@users.noreply.github.com>
2024-03-10 22:47:15 -0400

    Add files via upload

---
+++ Lab05_101349908/exercise-1/helloworld.ts
@@ -0,0 +1,6 @@
+var greeter = (firstName, lastName) => {
+    console.log(`Hello world ${firstName} ${lastName}`);
+  };
+  
+  greeter("Alvaro", "Aguirre");
+  
---
+++ Lab05_101349908/exercise-4/customer.ts
@@ -0,0 +1,18 @@
+export class Customer {
+    private firstName: string;
+    private lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    public greeter() {
+      console.log(`Hello ${this.firstName} ${this.lastName}`);
+    }
+  }
+  
+  // create instance of customer class and call method on object
+  let customer = new Customer("Alvaro", "Aguirre");
+
+  customer.greeter();
+  
